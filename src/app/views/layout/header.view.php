ddb85b31489eed5a492e6a5b98be756bee51054a
Da-nae <grosjean.danae@gmail.com>
2022-12-19 12:13:13 +0100

    add tailwind

--- src/app/views/layout/header.view.php
+++ src/app/views/layout/header.view.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link >
     <link href="/style/output.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
 <body>
     <header>
-
+        <h1 class="bg-pink-500">Hello</h1>
     </header>
--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,4 @@
 <?php
 declare(strict_types=1);
-
+require_once("app/views/layout/header.view.php");
 echo 'It works !';
