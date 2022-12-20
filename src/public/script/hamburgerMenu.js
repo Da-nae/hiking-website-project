71d124dacbc574c5ad9a3955680a884a9061f696
Da-nae <grosjean.danae@gmail.com>
2022-12-20 16:26:44 +0100

    hamburger menu

--- src/public/script/hamburgerMenu.js
+++ src/public/script/hamburgerMenu.js
@@ -1,1 +1,7 @@
-const button = document.querySelector();
+const button = document.querySelector('#button-menu');
+const menu = document.querySelector('#mobile-menu-2');
+
+button.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+});
+
