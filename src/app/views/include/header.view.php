4ce0fad0da4364720500c4dce1801efeec558ef4
Da-nae <grosjean.danae@gmail.com>
2022-12-20 16:52:31 +0100

    add user menu

--- src/app/views/include/header.view.php
+++ src/app/views/include/header.view.php
@@ -19,9 +19,9 @@
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">My hiking path</span>
                 </a>
                 <div class="flex items-center md:order-2">
-                    <button type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                    <button type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom" id="user-dropdown">
                         <span class="sr-only">Open user menu</span>
-                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo">
+                        <img class="w-8 h-8 rounded-full" src="assets/hiking.png" alt="user photo">
                     </button>
                     <!-- Dropdown menu -->
                     <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
@@ -31,13 +31,10 @@
                         </div>
                         <ul class="py-1" aria-labelledby="user-menu-button">
                             <li>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                                <a href="/settings" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
                             </li>
                             <li>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+                                <a href="/myhikes" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">My Hikes</a>
                             </li>
                             <li>
                                 <a href="/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
--- src/public/script/hamburgerMenu.js
+++ src/public/script/hamburgerMenu.js
@@ -5,3 +5,9 @@ button.addEventListener('click', () => {
     menu.classList.toggle('hidden');
 });
 
+const buttonUser = document.querySelector('#user-menu-button');
+const menuUser = document.querySelector('#user-dropdown');
+
+buttonUser.addEventListener('click', () => {
+    menuUser.classList.toggle('hidden');
+});
