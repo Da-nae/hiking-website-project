65c0ba610167673ba2b4f4db7111c966faa0a78f
Da-nae <grosjean.danae@gmail.com>
2022-12-21 14:32:58 +0100

    changed justify on tag buttons

--- src/app/views/tag.view.php
+++ src/app/views/tag.view.php
@@ -1,5 +1,5 @@
 <nav>
-    <ul class="flex mt-10 flex-wrap">
+    <ul class="flex mt-10 flex-wrap items-center justify-center">
     <?php foreach ($tags as $tag) : ?>
         <li class="mx-5 bg-Laurel-green text-white px-5 py-2 my-1 rounded-xl cursor-pointer">
         <a href="/tag?code=<?= $tag['ID_tags']?>"><?= $tag['tag_name'] ?></a>
