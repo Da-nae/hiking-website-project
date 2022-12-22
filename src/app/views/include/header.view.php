a79c02201f21f2ac17b7499ffb8f840d4c26bba5
Da-nae <grosjean.danae@gmail.com>
2022-12-22 10:06:32 +0100

    add styling

--- src/app/views/include/header.view.php
+++ src/app/views/include/header.view.php
@@ -20,10 +20,10 @@
                     <span class="self-center text-xl font-semibold whitespace-nowrap text-white">My hiking path</span>
                 </a>
                 <div class="flex items-center md:order-2">
-                    <p class="text-white">Welcome <?= $_SESSION['user']['username']; ?></p>
+                    <p class="text-white"><?= $_SESSION['user']['username']; ?></p>
                     <a href="/code?=<?= $_SESSION['user']['id']?>"><button type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom" id="user-dropdown">
                         <span class="sr-only">Open user menu</span>
-                        <img class="w-8 h-8 rounded-full" src="assets/hiking.png" alt="user photo">
+                        <img class="w-8 h-8 rounded-full ml-4" src="assets/hiking.png" alt="user photo">
                     </button>
                     </a>
                     
