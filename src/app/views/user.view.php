0f19fa6ba85eea5db688aa639aa2fdc6962432bf
Da-nae <grosjean.danae@gmail.com>
2022-12-23 10:29:39 +0100

    style userpage

--- src/app/views/user.view.php
+++ src/app/views/user.view.php
@@ -1,11 +1,13 @@
-<h1 class="text-center text-[45px] mt-16 font-bold">My account</h1>
-<div class="flex flex-col items-left w-[70%] m-auto ml-5 max-w-[600px] mb-7 border-2 border-Old-mauve shadow-lg py-8 pt-10 mt-2 rounded justify-center">
-    <p class="text-lg text-Tomato">Username:</p>
-    <p> <?= $_SESSION['user']['username']; ?></p>
-    <p class="text-lg text-Tomato">Firstname:</p>
-    <p> <?= $_SESSION['user']['firstname']; ?></p>
-    <p class="text-lg text-Tomato">Lastname:</p>
-    <p> <?= $_SESSION['user']['lastname']; ?></p>
-    <p class="text-lg text-Tomato">Email:</p>
-    <p> <?= $_SESSION['user']['email']; ?></p>
-</div>
+<article class="flex flex-col items-center justify-center">
+    <h1 class="text-center text-[45px] mt-16 font-bold">My account</h1>
+    <div class="flex flex-col w-[70%] m-auto pl-9 max-w-[600px] mb-7 border-2 border-Old-mauve shadow-lg py-8 pt-10 mt-2 rounded justify-center">
+        <p class="text-lg text-Tomato">Username:</p>
+        <p class="mt-1"> <?= $_SESSION['user']['username']; ?></p>
+        <p class="text-lg text-Tomato">Firstname:</p>
+        <p class="mt-1"> <?= $_SESSION['user']['firstname']; ?></p>
+        <p class="text-lg text-Tomato">Lastname:</p>
+        <p class="mt-1"> <?= $_SESSION['user']['lastname']; ?></p>
+        <p class="text-lg text-Tomato">Email:</p>
+        <p class="mt-1"> <?= $_SESSION['user']['email']; ?></p>
+    </div>
+</article>
